a7e0e68b0fc5a8a6ca728f376c20b10b8f266ce6
denysartiukhov <artiukhov.denys@gmail.com>
2022-11-14 16:52:10 -0600

    add kubectl to the instance user data script

--- terraform/aws/lab-k8s/entry-script.sh
+++ terraform/aws/lab-k8s/entry-script.sh
@@ -4,5 +4,7 @@ sudo sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 sudo usermod -aG docker $USER && newgrp docker
 #sudo minikube start --driver=docker
